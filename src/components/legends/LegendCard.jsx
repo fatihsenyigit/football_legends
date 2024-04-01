b00d95839a46c3331c05fdcd9eca4aeee6f0e514
fatih <fsenyigit8@gmail.com>
2024-04-01 13:54:06 +0300

    legend card and click the card functions

--- src/components/legends/LegendCard.jsx
+++ src/components/legends/LegendCard.jsx
@@ -1,11 +1,36 @@
-
+import { Card } from "react-bootstrap"
+import { useState } from "react"
 
 const LegendCard = ({info}) => {
+    const [show, setShow] = useState(false)
     return (
-        <div>
-            legendcard...
-        </div>
-    )
+      <Card
+        className="player-card"
+        role="button"
+        onClick={() => setShow(!show)}
+      >
+        {!show ? (
+          <Card.Img
+            variant="top"
+            src={info.img}
+            title={info.name}
+            alt={info.name}
+          ></Card.Img>
+        ) : (
+          <>
+            <Card.Header>
+              <Card.Title>{info.name}</Card.Title>
+            </Card.Header>
+            <ul className="m-auto">
+              {info.statistics.map((item) => (
+                <li className="list-unstyled h5 text-start"> ⚽ {item}</li>
+              ))}
+            </ul>
+            <span>{info.official_career}</span>
+          </>
+        )}
+      </Card>
+    );
 }
 
 export default LegendCard
--- src/components/legends/LegendContainer.jsx
+++ src/components/legends/LegendContainer.jsx
@@ -1,13 +1,20 @@
 
 
-import { Col, FormControl, Row } from "react-bootstrap";
+import { FormControl, Row } from "react-bootstrap";
 import { data } from "../../helpers/data"
 import Container from 'react-bootstrap/Container'
 import Form from "react-bootstrap/Form";
 import '../../index.css'
 import LegendCard from './LegendCard'
+import { useState } from "react";
 
 const LegendContainer = () => {
+    const [searchData, setSearchData] = useState('')
+    const handleSearch = (e) => {
+        setSearchData(e.target.value)
+    }
+    const filteredData = data
+    console.log(filteredData)
     return (
       <Container>
         <Form>
@@ -15,12 +22,13 @@ const LegendContainer = () => {
             className="legendInput border-info m-auto my-3"
             type="text"
             placeholder="search football players..."
+            onChange={handleSearch}
           ></FormControl>
         </Form>
 
         <Row xs={1} sm={2} md={3} lg={4} xl={5} className="justify-content-center g-3">
           {data.map((info) => {
-            return <LegendCard info={info}></LegendCard>;
+            return <LegendCard key={info.id} info={info}></LegendCard>;
           })}
         </Row>
       </Container>
